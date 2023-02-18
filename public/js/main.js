12521aa00c30970cd1c382679adb854199b0e329
Sh1vam6 <gautamshivam526@gmail.com>
2023-02-18 10:29:15 +0530

    express js project first commit

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+const day=document.getElementById("day");
+const today_date=document.getElementById("today_date");
+
+const city_name = document.getElementById('city_name');
+
+const temp_real_value = document.getElementById("temp_real_value");
+const temp_status = document.getElementById("temp_status");
+
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async (event) => {
+  event.preventDefault();
+  let cityVal = cityName.value;
+  if (cityVal === "") {
+    city_name.innerText = `Plz write the name of city before search`
+    datahide.classList.add('data_hide')
+
+  } else {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=302f98bdf010911bb7af00821f8716c9`
+
+      const response = await fetch(url);
+      const data = await response.json();
+      const arrData = [data];
+
+      city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+      temp_real_value.innerText = arrData[0].main.temp;
+
+      const tempMood = arrData[0].weather[0].main;
+
+      // condition to check sunny or cloudy
+      if (tempMood == "Clear") {
+        temp_status.innerHTML = `<i class="fa-solid fa-sun" style='color:#eccc68;'></i>`
+      }
+      else if (tempMood == "Clouds") {
+        temp_status.innerHTML = `<i  class="fa-solid fa-cloud" style='color:#f1f2f6;'></i>`
+      }
+      else if (tempMood == "Rain") {
+        temp_status.innerHTML = `<i class="fa-solid fa-raindrops style='color:#a4b0be'"></i>`
+      }
+      else {
+        temp_status.innerHTML = `<i class="fa-solid fa-sun" style='color:#eccc68;'></i>`
+      }
+      datahide.classList.remove('data_hide')
+
+    }
+    catch {
+      city_name.innerText = `Plz enter the city name properly`
+      datahide.classList.add('data_hide')
+
+    }
+  }
+}
+
+submitBtn.addEventListener('click', getInfo);
+
+const d=new Date();
+
+const day_date=()=>{
+  const weeks= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  day.innerText=weeks[d.getDay()];
+  today_date.innerText=d.getDate();
+}
+
+day_date();
---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const app=express();
+const path=require("path");
+const hbs=require('hbs')
+const port=process.env.PORT||3000; 
+
+const static_path=path.join(__dirname,"../public")
+const template_path=path.join(__dirname,"../templates/views")
+const partials_path=path.join(__dirname,"../templates/partials")
+
+app.set('view engine', 'hbs');
+app.set('views',template_path)
+
+hbs.registerPartials(partials_path)
+
+app.use(express.static(static_path))
+
+app.get("/",(req,res)=>{
+    // res.send("welcome to home page")
+    res.render('index')
+})
+
+app.get("/about",(req,res)=>{
+    // res.send("welcome to about page")
+    res.render('about')
+})
+
+app.get("/weather",(req,res)=>{
+    // res.send("welcome to weather page")
+    res.render('weather')
+})
+
+app.get("*",(req,res)=>{
+    // res.send("ERROR 404 PAGE")
+    res.render("404error",{
+        errorMsg:"OOPS! Page not found"
+    })
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port no ${port}`)
+})
